fc804683a66dadf5ffca27e4cde6d60d7e88f4ec
Nora <nsorym04@gmail.com>
2023-02-12 15:34:33 +0900

    paginationがうまく動かなかった不具合解消

--- wp-content/themes/tensha/category.php
+++ wp-content/themes/tensha/category.php
@@ -6,7 +6,8 @@
     <h1 class="subtitle"><?php echo $catname; ?><br><span>NEWS</span></h1>
 
     <ul class="news_page_list">
-        <?php query_posts('category_name=news'); ?>
+        <?php //query_posts('category_name=news'); 
+        ?>
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <li>
                     <h2><span><?php the_date('Y/m/d'); ?></span><br><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
@@ -14,12 +15,14 @@
                         <div class="news_page_thumbnail"><a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a></div>
                     <?php endif; ?>
                 </li>
-        <?php endwhile;
-        endif; ?>
+            <?php endwhile; ?>
+        <?php endif; ?>
     </ul>
+
     <?php if (function_exists('pagination')) {
         pagination($wp_query->max_num_pages);
-    }; ?>
+    };
+    ?>
 </main>
 
 <?php get_footer(); ?>
