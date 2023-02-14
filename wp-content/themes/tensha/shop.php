6066c0ebf5fafda8e4ba03f8f6a5b20ba339eef1
Nora <nsorym04@gmail.com>
2023-02-14 12:45:33 +0900

    ショップページ基本HTMLを読み込み

---
+++ wp-content/themes/tensha/shop.php
@@ -0,0 +1,92 @@
+<section class="info">
+    <div class="genre">
+        <div class="genre_in">
+            <img src="img/drink_list_white.png" alt="" /><span>飲む</span>
+        </div>
+        <div class="genre_in">
+            <img src="img/food_list_white.png" alt="" /><span>食べる</span>
+        </div>
+        <div class="genre_in">
+            <img src="img/play.png" alt="" /><span>遊ぶ</span>
+        </div>
+    </div>
+    <p class="info_txt">
+        串焼きが魅力の本店。<br />
+        熱々の鉄板で注文をいただいてからお焼きいたしますので、一番美味しい状態でお召し上がりいただけます。<br />
+        他にもドリンク、サブメニューもご用意しておりますのでぜひお越しください。<br />
+    </p>
+    <p class="info_times">
+        営業時間：午前10時から午後5時<br />
+        営業日：土日、祝日
+    </p>
+</section>
+<section class="shop_menu sec_padding">
+    <h1 class="subtitle">メニュー<br /><span>MENU</span></h1>
+    <div class="pickup_menu">
+        <div class="pickup_one">
+            <div class="pickup_img">
+                <img src="img/2023-01-29 12.35.46.jpg" alt="" />
+            </div>
+            <p>塩タン串焼き<span>600円</span></p>
+        </div>
+        <div class="pickup_one">
+            <div class="pickup_img">
+                <img src="img/2023-01-29 12.35.50.jpg" alt="" />
+            </div>
+            <p>塩タン串焼き<span>600円</span></p>
+        </div>
+        <div class="pickup_one">
+            <div class="pickup_img">
+                <img src="img/shop1_visu1.jpeg" alt="" />
+            </div>
+            <p>塩タン串焼き<span>600円</span></p>
+        </div>
+    </div>
+    <div class="other_menu">
+        <div class="other_menu_img">
+            <img src="img/shop1_menu.jpg" alt="" />
+        </div>
+        <dl class="other_menu_list">
+            <dl>
+                <dt>塩タン串焼き</dt>
+                <dd>600円</dd>
+            </dl>
+            <dl>
+                <dt>塩タン串焼き</dt>
+                <dd>600円</dd>
+            </dl>
+            <dl>
+                <dt>塩タン串焼き</dt>
+                <dd>600円</dd>
+            </dl>
+            <dl>
+                <dt>塩タン串焼き</dt>
+                <dd>600円</dd>
+            </dl>
+            <dl>
+                <dt>塩タン串焼き</dt>
+                <dd>600円</dd>
+            </dl>
+            <dl>
+                <dt>塩タン串焼き</dt>
+                <dd>600円</dd>
+            </dl>
+            <dl>
+                <dt>塩タン串焼き</dt>
+                <dd>600円</dd>
+            </dl>
+            <dl>
+                <dt>塩タン串焼き</dt>
+                <dd>600円</dd>
+            </dl>
+            <dl>
+                <dt>塩タン串焼き</dt>
+                <dd>600円</dd>
+            </dl>
+            <dl>
+                <dt>塩タン串焼き</dt>
+                <dd>600円</dd>
+            </dl>
+        </dl>
+    </div>
+</section>
--- wp-content/themes/tensha/single.php
+++ wp-content/themes/tensha/single.php
@@ -2,6 +2,8 @@
 <main>
     <?php if (in_category('news')) :; ?>
         <?php get_template_part('news'); ?>
+    <?php elseif (in_category('shops')) :; ?>
+        <?php get_template_part('shop'); ?>
     <?php endif; ?>
 </main>
 <?php get_footer(); ?>
